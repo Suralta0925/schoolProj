4509c2b33c5f6a1401f1813d5e292b31efd19128
vince <vinceiansuralta3@gmail.com>
2026-04-16 21:47:20 +0800

    feat: added create class and add subjects

--- src/service/class_schedule_service.py
+++ src/service/class_schedule_service.py
@@ -3,15 +3,21 @@ class ClassSchedule:
     def __init__(self,class_schedDB):
         self.class_schedDB = class_schedDB
 
-    def createClass(self, section, day, start, end, subject, room, instructor):
+    def createClass(self, section):
         classSchedModel = {
             "section": section,
-            "schedule": [{"day": day, "start": start, "end": end}],
-            "subject": subject,
-            "room": room,
-            "instructor": instructor
+            "subjects": []
+
         }
-        print(self.class_schedDB.save(classSchedModel))
+        print(self.class_schedDB.addData(classSchedModel))
+    def addSubjects(self, section, schedule_model):
+        data = self.class_schedDB.findOne("section", section)
+        if data:
+            data["subjects"].append(schedule_model)
+            self.class_schedDB.updateData("subjects",data["subjects"])
+            return "Successfully added a subject"
+        return f"Class {section} does not exists"
+
 
 
 
